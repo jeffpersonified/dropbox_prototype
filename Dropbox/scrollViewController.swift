0fdbf089618bd7b54c4d2ee276b3200fb8abc8d9
Jeff Smith <jeff@thoughtbot.com>
2014-09-15 21:26:58 -0700

    Add files

---
+++ Dropbox/scrollViewController.swift
@@ -0,0 +1,38 @@
+//
+//  scrollViewController.swift
+//  Dropbox
+//
+//  Created by Jeff Smith on 9/15/14.
+//  Copyright (c) 2014 Jeff Smith. All rights reserved.
+//
+
+import UIKit
+
+class scrollViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        scrollView.contentSize = CGSize(width: 320, height: 720)
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
